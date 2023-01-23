012824b4af5e2aa68e94c5734d5af2d31e8f95d6
Carlos Navarro <43856967+navarro165@users.noreply.github.com>
2023-01-22 22:41:11 -0600

    confirm with user that given path is correct

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from prompt_toolkit.completion import PathCompleter
 def main():
     completer = PathCompleter()
     src_dir = prompt("Enter the path of the source directory: ", completer=completer)
+
+    # Check if the user is ok with the given path
+    while True:
+        confirm = input(f"Is {src_dir} the correct path? (y/n)")
+        if confirm.lower() == 'y':
+            break
+        elif confirm.lower() == 'n':
+            src_dir = prompt("Enter the path of the source directory: ", completer=completer)
+        else:
+            print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+    
     organize_media(src_dir)
     print("Media files have been organized successfully.")
 
